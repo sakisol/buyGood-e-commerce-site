1f6fb13f8f9d374c18d4c97b49be07d45677bf2e
sakisol <sakipelita@gmail.com>
2019-04-10 20:42:00 +0200

    backbutton

--- js/main.js
+++ js/main.js
@@ -19,18 +19,9 @@ var app = new Vue({
         getItems: function () {
 
             var url = "https://api.globalgiving.org/api/public/projectservice/all/projects/active.json?api_key=f532b2d3-9dc1-428c-9f46-148656571409";
-
+        
             var newurl = url + this.nextprojID;
-            // var newurl = url = this.history[pageIndex]
-            // console.log(this.history[this.pageIndex])
-            
-            // newhead = new Headers();
-            
-            // newhead.append('Accept', 'application/json');
-            
-            console.log(this.pageIndex)
-            this.pageIndex++
-
+        
             let req = new Request(newurl, {
                 
             });
@@ -39,31 +30,51 @@ var app = new Vue({
                 return response.json();
                 
             }).then(function (data) {
-                console.log(data);
-                
-                app.history.push(app.nextprojID);
-                app.nextprojID = "&nextProjectId=" + data.projects.nextProjectId
-                console.log(app.nextprojID);
-                
-        
+                // console.log(data);   
                 // console.log(history)
                 
-
-                    for(let i=0; i < data.projects.project.length; i++){
-                        data.projects.project[i].quantity=0;
-                    }
-                   
-
-                    app.searchProjects = data.projects.project;
+                
+                for(let i=0; i < data.projects.project.length; i++){
+                    data.projects.project[i].quantity=0;
+                }
+                app.pageIndex++
+                
+                console.log(app.pageIndex);
+ 
+                if (app.pageIndex == (app.history.length+1)) {
+                    
+        
+                    app.nextprojID = "&nextProjectId=" + data.projects.nextProjectId
+                    app.history.push(data.projects.project);
+                    
+                    app.searchProjects = app.history[app.history.length-1]
                     console.log(app.searchProjects)
-                })
+                } else {
+        
+                    app.searchProjects = app.history[app.pageIndex-1]
+                }
+                 
+                    
+            })
                 .catch(function (error) {
-                    alert("error")
+                    // alert("error")
                     console.log("error", error);
                 });
             //end of fetch
-
+        
         }, //end of method getItems 
+        
+        getHistory(){
+
+            app.pageIndex--
+            app.searchProjects = app.history[app.pageIndex-1]
+            console.log(app.pageIndex);
+            console.log(app.history.length);
+            
+            
+        },
+
+
         goDetail: function (project) {
             this.selectedProj = project;
         },
@@ -82,9 +93,6 @@ var app = new Vue({
 
         },
         removefromBasket: function (selected) {
-            // selected.quantity -= 1;
-            // this.basket.splice(this.basket.indexOf(selected.id), 1);//this takes the whole project out of the basket 
-            //rather than one portion/donation amount
         
             var found = false;
             this.basket.forEach(proj => {
@@ -97,51 +105,8 @@ var app = new Vue({
                 selected.quantity = -1;
                 this.basket.push(selected);
             }
-        },
-        getHistory(){
-
-
-                var url = "https://api.globalgiving.org/api/public/projectservice/all/projects/active.json?api_key=f532b2d3-9dc1-428c-9f46-148656571409";
-    
-                var newurl = url + this.history[this.history.length - 1];
-                console.log(this.history[0])
-    
-                // newhead = new Headers();
-    
-                // newhead.append('Accept', 'application/json');
-    
-                let req = new Request(newurl, {
-            
-                });
-                fetch(req)
-                    .then(function (response) {
-                        return response.json();
-    
-                    }).then(function (data) {
-                        console.log(data);
-                        
-                        app.history.push(app.nextprojID);
-                        app.nextprojID = "&nextProjectId=" + data.projects.nextProjectId
-                        console.log(app.nextprojID);
-    
-                       
-                        // console.log(history)
-    
-    
-                        for(let i=0; i < data.projects.project.length; i++){
-                            data.projects.project[i].quantity=0;
-                        }
-                       
-    
-                        app.searchProjects = data.projects.project;
-                        console.log(app.searchProjects)
-                    })
-                    .catch(function (error) {
-                        alert("error")
-                        console.log("error", error);
-                    });
-                //end of fetch
-        } 
+        }
+       
     }, //end of methods
     created() {
         console.log("mounted!");
@@ -176,7 +141,7 @@ var app = new Vue({
             for (var i = 0; i < this.basket.length; i++){ 
             total += this.basket[i].donationOptions.donationOption[0].amount * this.basket[i].quantity;
                 };
-                 // get 10% discount when you donate to more than 5 projects  
+               
                 if (this.basket.length >= 3) {
                     let discount = total * 0.1;
                     total = total - discount;
@@ -194,7 +159,6 @@ var app = new Vue({
                 }         
             } 
       
-       
     }
 });
 
@@ -202,8 +166,3 @@ var app = new Vue({
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
-
-
-
-
-
