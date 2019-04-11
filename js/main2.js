94ce10c6d93cca78e9ca8a62377423b9ca89501d
sakisol <sakipelita@gmail.com>
2019-04-11 17:38:02 +0200

    working on media queiries

--- js/main2.js
+++ js/main2.js
@@ -37,20 +37,37 @@ var app = new Vue({
                 for(let i=0; i < data.projects.project.length; i++){
                     data.projects.project[i].quantity=0;
                 }
+                //ok so i added this pageIndex yesterday that is saving the current page visited. basically "next" add 1 and "back" is minus 1 on the index
                 app.pageIndex++
                 
                 console.log(app.pageIndex);
- 
+                
+                //so i use app.pageIndex and check if the current page is the last one, in which case i proceed to fetch new data and asign the next project id
+                //THIS NEEDS TO BE REFACTORED! the fetch should be inside the IF and not the other way around so this way if you go forward on a page that already
+                //has been loaded, you dont fetch and thus you dont query the API and avoid using promises
                 if (app.pageIndex == (app.history.length+1)) {
                     
         
                     app.nextprojID = "&nextProjectId=" + data.projects.nextProjectId
+                    //here instead of saving the proyect IDs i save the entire project object as a new entry in the array History
                     app.history.push(data.projects.project);
                     
+                    // console.log(app.nextprojID);
+                    // console.log(app.history[0]);
+                    
+                    
+                    // console.log(app.history[app.history.length-1]);
+                    
+                    // console.log(app.history.length);
+                    // console.log(app.pageIndex == app.history.length);
+        
+                    //then asign searchProjects to the latest entry in the array
                     app.searchProjects = app.history[app.history.length-1]
                     console.log(app.searchProjects)
                 } else {
         
+                    //If the current pageIndex is not the latest page, we save the corresponding entry from the history into searchProjects
+                    //the back button is basically the same but reverse and without the fetch bit
                     app.searchProjects = app.history[app.pageIndex-1]
                 }
                  
@@ -93,6 +110,9 @@ var app = new Vue({
 
         },
         removefromBasket: function (selected) {
+            // selected.quantity -= 1;
+            // this.basket.splice(this.basket.indexOf(selected.id), 1);//this takes the whole project out of the basket 
+            //rather than one portion/donation amount
         
             var found = false;
             this.basket.forEach(proj => {
@@ -104,9 +124,6 @@ var app = new Vue({
             if (found === false) {
                 selected.quantity = -1;
                 this.basket.push(selected);
-            }
-            else if ( selected.quantity === 0){
-                this.basket.splice(this.basket.indexOf(selected.id), 1);
             }
         }
        
@@ -144,7 +161,7 @@ var app = new Vue({
             for (var i = 0; i < this.basket.length; i++){ 
             total += this.basket[i].donationOptions.donationOption[0].amount * this.basket[i].quantity;
                 };
-               
+                 // get 10% discount when you donate to more than 5 projects  
                 if (this.basket.length >= 3) {
                     let discount = total * 0.1;
                     total = total - discount;
@@ -162,6 +179,7 @@ var app = new Vue({
                 }         
             } 
       
+       
     }
 });
 
@@ -169,3 +187,8 @@ var app = new Vue({
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+
+
+
